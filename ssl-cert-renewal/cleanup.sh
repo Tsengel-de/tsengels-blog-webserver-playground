35391fc955fa8fb0bc7efa80d8d344359f2dd716
tsengel <tsengel.de@gmail.com>
2024-08-02 23:23:29 +0200

    request the acme-challenge records

--- ssl-cert-renewal/cleanup.sh
+++ ssl-cert-renewal/cleanup.sh
@@ -9,4 +9,12 @@ API_KEY_HEADER="X-API-Key: $API_KEY"
 if [ -f /tmp/CERTBOT_$CERTBOT_DOMAIN ]; then
     ZONE_ID=$(cat /tmp/CERTBOT_$CERTBOT_DOMAIN)
     rm -f /tmp/CERTBOT_$CERTBOT_DOMAIN
+
+    CREATE_DOMAIN="_acme-challenge.$CERTBOT_DOMAIN"
+    # request the created records
+    RECORD_GET_RESPONSE=$(curl -s -X GET "$API_URL/zones/$ZONE_ID?recordName=$CREATE_DOMAIN&recordType=TXT" \
+                             -H "$API_KEY_HEADER" \
+                             -H "Accept: application/json")
+    RECORD_IDS=$(echo $RECORD_GET_RESPONSE \
+            | python -c "import sys,json;records=json.load(sys.stdin)['records'];print('\n'.join([x['id'] for x in records]))")
 fi
