cc67a459557d5874c0f6ecf66c5e44a144872dc2
tsengel <tsengel.de@gmail.com>
2024-08-02 23:06:42 +0200

    if already tld then use it

--- ssl-cert-renewal/authenticator.sh
+++ ssl-cert-renewal/authenticator.sh
@@ -9,3 +9,8 @@ set +a
 
 # strip only the top domain to get the zone id
 ZONE_NAME=$(expr match "$CERTBOT_DOMAIN" '.*\.\(.*\..*\)')
+
+# when already the top level domain then use it
+if [ -z "$ZONE_NAME" ]; then
+  ZONE_NAME="$CERTBOT_DOMAIN"
+fi
