9993054750550d0c2c991e0e60e984dcd41fd119
tsengel <tsengel.de@gmail.com>
2024-08-02 23:19:40 +0200

    when the file for clean-up exists get data and remove it

--- ssl-cert-renewal/cleanup.sh
+++ ssl-cert-renewal/cleanup.sh
@@ -5,3 +5,8 @@ set -x
 
 API_URL="https://api.hosting.ionos.com/dns/v1"
 API_KEY_HEADER="X-API-Key: $API_KEY"
+
+if [ -f /tmp/CERTBOT_$CERTBOT_DOMAIN ]; then
+    ZONE_ID=$(cat /tmp/CERTBOT_$CERTBOT_DOMAIN)
+    rm -f /tmp/CERTBOT_$CERTBOT_DOMAIN
+fi
