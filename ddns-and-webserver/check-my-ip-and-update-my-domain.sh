af43866b5e0a904811263638d55acd78d029b5f9
tsengel <tsengel.de@gmail.com>
2024-08-05 22:04:31 +0200

    update ip in text file & add new ssh-key usage for minion, :D

--- ddns-and-webserver/check-my-ip-and-update-my-domain.sh
+++ ddns-and-webserver/check-my-ip-and-update-my-domain.sh
@@ -18,7 +18,9 @@ NEW_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 # compare the IPs
 if [ $NEW_IP != $OLD_IP ];
   # when the ip is different update in intranet routing in Router mikrotik chateau 5g ax
-  then ssh -i /home/tsengel/ admin@$ROUTER_IP "/ip firewall nat add chain=dstnat action=dst-nat dst-address=$NEW_IP to-address=$WEBSERVER_IP protocol=tcp dst-port=443; /ip firewall nat add chain=srcnat action=masquerade dst-address=$WEBSERVER_IP src-address=$VLAN_RANGE protocol=tcp dst-port=443"
+  then ssh -i /opt/minion/minion admin@$ROUTER_IP "/ip firewall nat add chain=dstnat action=dst-nat dst-address=$NEW_IP to-address=$WEBSERVER_IP protocol=tcp dst-port=443; /ip firewall nat add chain=srcnat action=masquerade dst-address=$WEBSERVER_IP src-address=$VLAN_RANGE protocol=tcp dst-port=443"
   # then update IP by ionos DNS
   curl -X GET "https://ipv4.api.hosting.ionos.com/dns/v1/${DDNS_QUERY}"
+  # update ip in ip.txt
+  echo $NEW_IP > ip.txt
 fi
