82850347ae06750b41d0039ea1b9f852b15d50f9
tsengel <tsengel.de@gmail.com>
2024-08-06 03:49:54 +0200

    activate debugging

--- ddns-and-webserver/check-my-ip-and-update-my-domain.sh
+++ ddns-and-webserver/check-my-ip-and-update-my-domain.sh
@@ -2,7 +2,7 @@
 
 # we need to install dnsutils for checking our wan ip
 # sudo apt install dnsutils
-
+set -x
 # your ssh-pub-key must be added in your router settings
 
 # read file and export environment variables
@@ -18,7 +18,7 @@ NEW_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 # compare the IPs
 if [ $NEW_IP != $OLD_IP ];
   # when the ip is different update in intranet routing in Router mikrotik chateau 5g ax
-  then ssh -i /opt/minion/minion admin@$ROUTER_IP "/ip firewall nat add chain=dstnat action=dst-nat dst-address=$NEW_IP to-address=$WEBSERVER_IP protocol=tcp dst-port=443; /ip firewall nat add chain=srcnat action=masquerade dst-address=$WEBSERVER_IP src-address=$VLAN_RANGE protocol=tcp dst-port=443"
+  then ssh -i /home/tsengel/.ssh/tsengel_everywhere admin@$ROUTER_IP "/ip firewall nat add chain=dstnat action=dst-nat dst-address=$NEW_IP to-address=$WEBSERVER_IP protocol=tcp dst-port=443; /ip firewall nat add chain=srcnat action=masquerade dst-address=$WEBSERVER_IP src-address=$VLAN_RANGE protocol=tcp dst-port=443"
   # then update IP by ionos DNS
   curl -X GET "https://ipv4.api.hosting.ionos.com/dns/v1/${DDNS_QUERY}"
   # update ip in ip.txt
