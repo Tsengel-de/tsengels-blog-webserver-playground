61760a466bcd9490c09fa9efae70e312b9877338
tsengel <tsengel.de@gmail.com>
2025-12-31 20:33:38 +0100

    feat(ddns): add cluster port forwarding (8443) to auto-update script

--- ddns-and-webserver/check-my-ip-and-update-my-domain.sh
+++ ddns-and-webserver/check-my-ip-and-update-my-domain.sh
@@ -18,7 +18,9 @@ NEW_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 # compare the IPs
 if [ $NEW_IP != $OLD_IP ];
   # when the ip is different update in intranet routing in Router mikrotik chateau 5g ax
-  then ssh -i /home/tsengel/.ssh/tsengel_everywhere admin@$ROUTER_IP "/ip firewall nat add chain=dstnat action=dst-nat dst-address=$NEW_IP to-address=$WEBSERVER_IP protocol=tcp dst-port=443; /ip firewall nat add chain=srcnat action=masquerade dst-address=$WEBSERVER_IP src-address=$VLAN_RANGE protocol=tcp dst-port=443"
+  then
+    CLUSTER_IP="192.168.88.150"
+    ssh -i /home/tsengel/.ssh/tsengel_everywhere admin@$ROUTER_IP "/ip firewall nat add chain=dstnat action=dst-nat dst-address=$NEW_IP to-address=$WEBSERVER_IP protocol=tcp dst-port=443; /ip firewall nat add chain=srcnat action=masquerade dst-address=$WEBSERVER_IP src-address=$VLAN_RANGE protocol=tcp dst-port=443; /ip firewall nat add chain=dstnat action=dst-nat dst-address=$NEW_IP to-address=$CLUSTER_IP to-ports=443 protocol=tcp dst-port=8443"
   # then update IP by ionos DNS
   curl -X GET "https://ipv4.api.hosting.ionos.com/dns/v1/${DDNS_QUERY}"
   # update ip in ip.txt
