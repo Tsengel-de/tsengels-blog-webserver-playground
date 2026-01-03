946b835b0ad51acc317c2d299d07f77b5ff70259
tsengel <tsengel.de@gmail.com>
2026-01-03 12:26:03 +0100

    feat: add homelab wildcard update to ddns script

--- ddns-and-webserver/check-my-ip-and-update-my-domain.sh
+++ ddns-and-webserver/check-my-ip-and-update-my-domain.sh
@@ -23,6 +23,15 @@ if [ $NEW_IP != $OLD_IP ];
     ssh -i /home/tsengel/.ssh/tsengel_everywhere admin@$ROUTER_IP "/ip firewall nat add chain=dstnat action=dst-nat dst-address=$NEW_IP to-address=$WEBSERVER_IP protocol=tcp dst-port=443; /ip firewall nat add chain=srcnat action=masquerade dst-address=$WEBSERVER_IP src-address=$VLAN_RANGE protocol=tcp dst-port=443; /ip firewall nat add chain=dstnat action=dst-nat dst-address=$NEW_IP to-address=$CLUSTER_IP to-ports=443 protocol=tcp dst-port=8443"
   # then update IP by ionos DNS
   curl -X GET "https://ipv4.api.hosting.ionos.com/dns/v1/${DDNS_QUERY}"
+
+  # Update *.homelab.tsengel.de via Ionos API (using IDs for stability)
+  # Zone ID: c8487622-f7c2-11eb-9fc9-0a5864441a29
+  # Record ID: efb444bb-4e71-9f2a-fc57-800930ddacfc
+  curl -s -X PUT "$API_URL/zones/c8487622-f7c2-11eb-9fc9-0a5864441a29/records/efb444bb-4e71-9f2a-fc57-800930ddacfc" \
+       -H "$API_KEY_HEADER" \
+       -H "Content-Type: application/json" \
+       --data '{"name": "*.homelab.tsengel.de", "type": "A", "content": "'"$NEW_IP"'", "ttl": 3600}'
+
   # update ip in ip.txt
   echo $NEW_IP > ip.txt
 fi
