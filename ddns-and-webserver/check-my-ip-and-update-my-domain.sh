f7ce0de2b22f75451d17f4ec08d04a3bb905081d
tsengel <tsengel.de@gmail.com>
2026-03-08 23:11:49 +0100

    Auto-commit: sync local changes to GitHub

--- ddns-and-webserver/check-my-ip-and-update-my-domain.sh
+++ ddns-and-webserver/check-my-ip-and-update-my-domain.sh
@@ -16,23 +16,47 @@ OLD_IP=$(cat ip.txt)
 NEW_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 
 # compare the IPs
-if [ $NEW_IP != $OLD_IP ];
-  # when the ip is different update in intranet routing in Router mikrotik chateau 5g ax
-  then
+if [ "$NEW_IP" != "$OLD_IP" ]; then
     CLUSTER_IP="192.168.88.150"
     ssh -i /home/tsengel/.ssh/tsengel_everywhere admin@$ROUTER_IP "/ip firewall nat add chain=dstnat action=dst-nat dst-address=$NEW_IP to-address=$WEBSERVER_IP protocol=tcp dst-port=443; /ip firewall nat add chain=srcnat action=masquerade dst-address=$WEBSERVER_IP src-address=$VLAN_RANGE protocol=tcp dst-port=443; /ip firewall nat add chain=dstnat action=dst-nat dst-address=$NEW_IP to-address=$CLUSTER_IP to-ports=443 protocol=tcp dst-port=8443"
-  # then update IP by ionos DNS
-  curl -X GET "https://ipv4.api.hosting.ionos.com/dns/v1/${DDNS_QUERY}"
 
-  # Update *.homelab.tsengel.de via Ionos API (using IDs for stability)
-  # Zone ID: c8487622-f7c2-11eb-9fc9-0a5864441a29
-  # Record ID: efb444bb-4e71-9f2a-fc57-800930ddacfc
-  curl -s -X PUT "$API_URL/zones/c8487622-f7c2-11eb-9fc9-0a5864441a29/records/efb444bb-4e71-9f2a-fc57-800930ddacfc" \
-       -H "$API_KEY_HEADER" \
-       -H "Content-Type: application/json" \
-       --data '{"name": "*.homelab.tsengel.de", "type": "A", "content": "'"$NEW_IP"'", "ttl": 3600}'
+    # ------------------------------------------------------------------
+    # IONOS DNS API UPDATES
+    # ------------------------------------------------------------------
+    # We use specific Record IDs to update ONLY the desired domains.
+    # The Zone ID is hardcoded for tsengel.de
+    ZONE_ID="c8487622-f7c2-11eb-9fc9-0a5864441a29"
 
-  # update ip in ip.txt
-  echo $NEW_IP > ip.txt
+    # 1. Update *.homelab.tsengel.de
+    curl -s -X PUT "$API_URL/zones/$ZONE_ID/records/efb444bb-4e71-9f2a-fc57-800930ddacfc" \
+         -H "$API_KEY_HEADER" \
+         -H "Content-Type: application/json" \
+         --data '{"name": "*.homelab.tsengel.de", "type": "A", "content": "'"$NEW_IP"'", "ttl": 3600}'
+
+    # 2. Update blog.tsengel.de (ID: d9dcf431-bf54-53e2-8c33-2eaf174fb32b)
+    curl -s -X PUT "$API_URL/zones/$ZONE_ID/records/d9dcf431-bf54-53e2-8c33-2eaf174fb32b" \
+         -H "$API_KEY_HEADER" \
+         -H "Content-Type: application/json" \
+         --data '{"name": "blog.tsengel.de", "type": "A", "content": "'"$NEW_IP"'", "ttl": 3600}'
+
+    # 3. Update www.blog.tsengel.de (ID: f8265fa0-7f78-ac66-5577-7af1f36e1eee)
+    curl -s -X PUT "$API_URL/zones/$ZONE_ID/records/f8265fa0-7f78-ac66-5577-7af1f36e1eee" \
+         -H "$API_KEY_HEADER" \
+         -H "Content-Type: application/json" \
+         --data '{"name": "www.blog.tsengel.de", "type": "A", "content": "'"$NEW_IP"'", "ttl": 3600}'
+
+    # 4. Update www.tsengel.de (ID: d07fd6e0-869d-348e-57c3-b204e2ed2b5e)
+    curl -s -X PUT "$API_URL/zones/$ZONE_ID/records/d07fd6e0-869d-348e-57c3-b204e2ed2b5e" \
+         -H "$API_KEY_HEADER" \
+         -H "Content-Type: application/json" \
+         --data '{"name": "www.tsengel.de", "type": "A", "content": "'"$NEW_IP"'", "ttl": 3600}'
+
+    # 5. Update tsengel.de (Root @) (ID: 651a4bb3-2283-fc22-7e56-9b9f82a17034)
+    curl -s -X PUT "$API_URL/zones/$ZONE_ID/records/651a4bb3-2283-fc22-7e56-9b9f82a17034" \
+         -H "$API_KEY_HEADER" \
+         -H "Content-Type: application/json" \
+         --data '{"name": "tsengel.de", "type": "A", "content": "'"$NEW_IP"'", "ttl": 3600}'
+
+    # update ip in ip.txt
+    echo $NEW_IP > ip.txt
 fi
-
